530c2a7b4a7ab3ae45a7be13aaaef8b34a3dafa2
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-23 11:16:12 +0500

    Update destination_setup_table.php

--- destination_setup_table.php
+++ destination_setup_table.php
@@ -304,9 +304,9 @@ if (isset($_POST['btnadd'])) {
   <div class="breadCrumb_bar_iner">
     <div class="">
         <div class="btn-group btn-breadcrumb">
-          <a href="#" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
-          <a href="Usermodules.php" class="btn btn-info">Setup</a>
-          <a href="hr_add_emp_info.php" class="btn btn-info active">Destination Setup Table</a>
+          <a href="usermodules.php" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
+          <a href="usermodules.php" class="btn btn-info">Setup</a>
+          <a href="destination_setup_table" class="btn btn-info active">Destination Setup Table</a>
         </div>
     </div>
   </div>
