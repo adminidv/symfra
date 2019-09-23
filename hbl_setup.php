f4e67427279a39002beeb199afcefa0575b06862
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-23 11:17:55 +0500

    Update hbl_setup.php

--- hbl_setup.php
+++ hbl_setup.php
@@ -278,9 +278,9 @@ if (isset($_POST['btnadd'])) {
   <div class="breadCrumb_bar_iner">
     <div class="">
         <div class="btn-group btn-breadcrumb">
-          <a href="#" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
-          <a href="Usermodules.php" class="btn btn-info">Setups</a>
-          <a href="hr_add_emp_info.php" class="btn btn-info active">Hbl Setup</a>
+          <a href="usermodules.php" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
+          <a href="usermodules.php" class="btn btn-info">Setups</a>
+          <a href="hbl_setup.php" class="btn btn-info active">Hbl Setup</a>
         </div>
     </div>
   </div>
