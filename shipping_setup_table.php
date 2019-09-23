b02376762523f2d9bfedb6a979b6a6ca953f54e0
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-23 11:21:19 +0500

    Update shipping_setup_table.php

--- shipping_setup_table.php
+++ shipping_setup_table.php
@@ -190,9 +190,9 @@ if (isset($_POST['btnadd'])) {
   <div class="breadCrumb_bar_iner">
     <div class="">
         <div class="btn-group btn-breadcrumb">
-          <a href="#" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
-          <a href="Usermodules.php" class="btn btn-info">Setup</a>
-          <a href="hr_add_emp_info.php" class="btn btn-info active">Shipping Line Setup Table</a>
+          <a href="usermodules.php" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
+          <a href="usermodules.php" class="btn btn-info">Setup</a>
+          <a href="shipping_setup_table.php" class="btn btn-info active">Shipping Line Setup Table</a>
         </div>
     </div>
   </div>
