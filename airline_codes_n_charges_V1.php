7aeadb1973fb385ab1fc81d863569c472ba2d3f2
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-23 11:11:44 +0500

    Update airline_codes_n_charges_V1.php

--- airline_codes_n_charges_V1.php
+++ airline_codes_n_charges_V1.php
@@ -301,8 +301,8 @@ if (isset($_POST['btnadd1'])) {
     <div class="">
         <div class="btn-group btn-breadcrumb">
           <a href="usermodules.php" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
-          <a href="Usermodules.php" class="btn btn-info">Setups</a>
-          <a href="hr_add_emp_info.php" class="btn btn-info active">Airline Codes & Charges</a>
+          <a href="usermodules.php" class="btn btn-info">Setups</a>
+          <a href="airline_codes_n_charges.php" class="btn btn-info active">Airline Codes & Charges</a>
         </div>
     </div>
   </div>
