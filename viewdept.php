c6ba80e72a36abf0c0605a825662cfa72b6c79c7
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-23 11:22:03 +0500

    Update viewdept.php

--- viewdept.php
+++ viewdept.php
@@ -107,9 +107,9 @@ if(isset($_POST["btnDelete"]))
   <div class="breadCrumb_bar_iner">
     <div class="">
         <div class="btn-group btn-breadcrumb">
-          <a href="#" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
-          <a href="Usermodules.php" class="btn btn-info">Human Resource</a>
-          <a href="hr_add_emp_info.php" class="btn btn-info active">View Department</a>
+          <a href="usermodules.php" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
+          <a href="usermodules.php" class="btn btn-info">Human Resource</a>
+          <a href="viewdept.php" class="btn btn-info active">View Department</a>
         </div>
     </div>
   </div>
