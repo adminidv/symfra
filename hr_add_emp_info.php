89e92e6626a647fde8cc345960205a965770da00
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-23 11:17:57 +0500

    Update hr_add_emp_info.php

--- hr_add_emp_info.php
+++ hr_add_emp_info.php
@@ -340,8 +340,8 @@ if(isset($_POST['saveBtn']))
   <div class="breadCrumb_bar_iner">
     <div class="">
         <div class="btn-group btn-breadcrumb">
-          <a href="#" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
-          <a href="Usermodules.php" class="btn btn-info">Human Resource</a>
+          <a href="usermodules.php" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
+          <a href="usermodules.php" class="btn btn-info">Human Resource</a>
           <a href="hr_add_emp_info.php" class="btn btn-info active">Add Employee</a>
         </div>
     </div>
