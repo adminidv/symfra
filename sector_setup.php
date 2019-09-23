eac3b2f0bdfd0775d4df654c73008f55b1d0ba48
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-23 11:20:59 +0500

    Update sector_setup.php

--- sector_setup.php
+++ sector_setup.php
@@ -282,9 +282,9 @@ if (isset($_POST['btnadd'])) {
   <div class="breadCrumb_bar_iner">
     <div class="">
         <div class="btn-group btn-breadcrumb">
-          <a href="#" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
-          <a href="Usermodules.php" class="btn btn-info">Setup</a>
-          <a href="hr_add_emp_info.php" class="btn btn-info active">Sector Setup Table</a>
+          <a href="usermodules.php" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
+          <a href="usermodules.php" class="btn btn-info">Setup</a>
+          <a href="sector_setup.php" class="btn btn-info active">Sector Setup Table</a>
         </div>
     </div>
   </div>
