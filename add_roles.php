bd7137d5a69b34593c198d1ce136520f81ef3adf
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-23 11:11:17 +0500

    Update add_roles.php

--- add_roles.php
+++ add_roles.php
@@ -71,7 +71,7 @@ if(isset($_POST['submitBtn']))
     <div class="">
         <div class="btn-group btn-breadcrumb">
           <a href="#" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
-          <a href="Usermodules.php" class="btn btn-info">User Management</a>
+          <a href="usermodules.php" class="btn btn-info">User Management</a>
           <a href="add_roles.php" class="btn btn-info active">Add Roles</a>
         </div>
     </div>
