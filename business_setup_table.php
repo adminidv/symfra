5e442867c1dce1bdf237874926e63bc8491076a7
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-23 11:14:16 +0500

    Update business_setup_table.php

--- business_setup_table.php
+++ business_setup_table.php
@@ -290,8 +290,8 @@ if (isset($_POST['btnadd'])) {
     <div class="">
         <div class="btn-group btn-breadcrumb">
           <a href="#" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
-          <a href="Usermodules.php" class="btn btn-info">Setup</a>
-          <a href="hr_add_emp_info.php" class="btn btn-info active">Business Setup Table</a>
+          <a href="usermodules.php" class="btn btn-info">Setup</a>
+          <a href="business_setup_table.php" class="btn btn-info active">Business Setup Table</a>
         </div>
     </div>
   </div>
