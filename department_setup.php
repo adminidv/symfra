dd4599c6fc292f5f5d6e5789c891fac95d151c7d
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-23 11:16:07 +0500

    Update department_setup.php

--- department_setup.php
+++ department_setup.php
@@ -196,9 +196,9 @@ if (isset($_POST['btnadd'])) {
   <div class="breadCrumb_bar_iner">
     <div class="">
         <div class="btn-group btn-breadcrumb">
-          <a href="#" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
-          <a href="Usermodules.php" class="btn btn-info">Setup</a>
-          <a href="hr_add_emp_info.php" class="btn btn-info active">Department Setup Table</a>
+          <a href="usermodules.php" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
+          <a href="usermodules.php" class="btn btn-info">Setup</a>
+          <a href="#" class="btn btn-info active">Department Setup Table</a>
         </div>
     </div>
   </div>
