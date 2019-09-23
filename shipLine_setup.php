51b7e5dc338e5d4cdb7b8b8cf5df2c9d5f588dd8
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-23 11:21:04 +0500

    Update shipLine_setup.php

--- shipLine_setup.php
+++ shipLine_setup.php
@@ -159,8 +159,8 @@ if (isset($_POST['submitBtn'])) {
     <div class="">
         <div class="btn-group btn-breadcrumb">
           <a href="usermodules.php" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
-          <a href="Usermodules.php" class="btn btn-info">Setups</a>
-          <a href="hr_add_emp_info.php" class="btn btn-info active">Shipping Line Code</a>
+          <a href="usermodules.php" class="btn btn-info">Setups</a>
+          <a href="shipLine_setup.php" class="btn btn-info active">Shipping Line Code</a>
         </div>
     </div>
   </div>
