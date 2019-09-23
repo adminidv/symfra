2c0c3fa15a3f0266528cb28b25c794299e565119
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-23 11:12:53 +0500

    Update airport_setup_View.php

--- airport_setup_View.php
+++ airport_setup_View.php
@@ -155,8 +155,8 @@ $userNo = $_GET['id'];
     <div class="">
         <div class="btn-group btn-breadcrumb">
           <a href="usermodules.php" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
-          <a href="Usermodules.php" class="btn btn-info">Setups</a>
-          <a href="hr_add_emp_info.php" class="btn btn-info active">Airport Setup</a>
+          <a href="usermodules.php" class="btn btn-info">Setups</a>
+          <a href="#" class="btn btn-info active">Airport Setup</a>
         </div>
     </div>
   </div>
