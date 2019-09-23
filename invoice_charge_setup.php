03655b3998acb37051bf7dc971470ba69ff59ae4
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-23 11:18:08 +0500

    Update invoice_charge_setup.php

--- invoice_charge_setup.php
+++ invoice_charge_setup.php
@@ -188,9 +188,9 @@ if (isset($_POST['btnadd'])) {
   <div class="breadCrumb_bar_iner">
     <div class="">
         <div class="btn-group btn-breadcrumb">
-          <a href="#" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
-          <a href="Usermodules.php" class="btn btn-info">Setup</a>
-          <a href="hr_add_emp_info.php" class="btn btn-info active">Invoice Setup Table</a>
+          <a href="usermodules.php" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
+          <a href="usermodules.php" class="btn btn-info">Setup</a>
+          <a href="#" class="btn btn-info active">Invoice Setup Table</a>
         </div>
     </div>
   </div>
