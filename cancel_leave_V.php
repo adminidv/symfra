2e92323d306cc65286b6be7901012ee65f059183
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-23 11:14:14 +0500

    Update cancel_leave_V.php

--- cancel_leave_V.php
+++ cancel_leave_V.php
@@ -85,8 +85,8 @@ $userNo = $_GET['SrNo'];
     <div class="">
         <div class="btn-group btn-breadcrumb">
           <a href="#" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
-          <a href="Usermodules.php" class="btn btn-info">User Management</a>
-          <a href="add_department.php" class="btn btn-info active">Add Department</a>
+          <a href="usermodules.php" class="btn btn-info">User Management</a>
+          <a href="#" class="btn btn-info active">Add Department</a>
         </div>
     </div>
   </div>
