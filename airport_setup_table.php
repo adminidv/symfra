b420311d4c3cedc2ea4f5728bfdcd264a3ba3f03
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-23 11:14:08 +0500

    Update airport_setup_table.php

--- airport_setup_table.php
+++ airport_setup_table.php
@@ -291,9 +291,9 @@ if (isset($_POST['btnadd'])) {
   <div class="breadCrumb_bar_iner">
     <div class="">
         <div class="btn-group btn-breadcrumb">
-          <a href="#" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
-          <a href="Usermodules.php" class="btn btn-info">Setup</a>
-          <a href="hr_add_emp_info.php" class="btn btn-info active">Airport Setup Table</a>
+          <a href="usermodules.php" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
+          <a href="usermodules.php" class="btn btn-info">Setup</a>
+          <a href="airport_setup_table.php" class="btn btn-info active">Airport Setup Table</a>
         </div>
     </div>
   </div>
