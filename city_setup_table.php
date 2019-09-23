d3ff3a4e6b740f2625242bff63d94cee213febbe
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-23 11:14:23 +0500

    Update city_setup_table.php

--- city_setup_table.php
+++ city_setup_table.php
@@ -194,9 +194,9 @@ if (isset($_POST['submitBtn'])) {
   <div class="breadCrumb_bar_iner">
     <div class="">
         <div class="btn-group btn-breadcrumb">
-          <a href="#" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
-          <a href="Usermodules.php" class="btn btn-info">Setup</a>
-          <a href="hr_add_emp_info.php" class="btn btn-info active">City Setup Table</a>
+          <a href="usermodules.php" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
+          <a href="usermodules.php" class="btn btn-info">Setup</a>
+          <a href="city_setup_table.php" class="btn btn-info active">City Setup Table</a>
         </div>
     </div>
   </div>
