14028a8a7465c01f0c8ea4180a3fb91ff3608b80
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-23 11:14:37 +0500

    Update clearing_agents_VE.php

--- clearing_agents_VE.php
+++ clearing_agents_VE.php
@@ -350,8 +350,8 @@ if ($viewvar==$row['SrNo'])
     <div class="">
         <div class="btn-group btn-breadcrumb">
           <a href="usermodules.php" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
-          <a href="Usermodules.php" class="btn btn-info">Setups</a>
-          <a href="hr_add_emp_info.php" class="btn btn-info active">View Clearing Agents</a>
+          <a href="usermodules.php" class="btn btn-info">Setups</a>
+          <a href="#" class="btn btn-info active">View Clearing Agents</a>
         </div>
     </div>
   </div>
