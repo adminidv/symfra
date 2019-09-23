eabb26d6ae28cd0fe97c271bed1c33f892d5d114
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-23 11:20:48 +0500

    Update sea_export_table.php

--- sea_export_table.php
+++ sea_export_table.php
@@ -1083,9 +1083,9 @@ else
   <div class="breadCrumb_bar_iner">
     <div class="">
         <div class="btn-group btn-breadcrumb">
-          <a href="#" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
-          <a href="Usermodules.php" class="btn btn-info">CRM</a>
-          <a href="hr_add_emp_info.php" class="btn btn-info active">Sea Export Table</a>
+          <a href="usermodules.php" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
+          <a href="usermodules.php" class="btn btn-info">CRM</a>
+          <a href="sea_export_table.php" class="btn btn-info active">Sea Export Table</a>
         </div>
     </div>
   </div>
