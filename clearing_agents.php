63b2ca66cbfc55bbc18fa4f2c6ee0cd0efa5484f
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-23 11:14:29 +0500

    Update clearing_agents.php

--- clearing_agents.php
+++ clearing_agents.php
@@ -159,8 +159,8 @@ if (isset($_POST['submitBtn'])) {
     <div class="">
         <div class="btn-group btn-breadcrumb">
           <a href="usermodules.php" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
-          <a href="Usermodules.php" class="btn btn-info">Setups</a>
-          <a href="hr_add_emp_info.php" class="btn btn-info active">Clearing Agents</a>
+          <a href="usermodules.php" class="btn btn-info">Setups</a>
+          <a href="clearing_agents.php" class="btn btn-info active">Clearing Agents</a>
         </div>
     </div>
   </div>
