6bd2d9718a239eac5153d669716f56cca9873b45
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-23 11:19:59 +0500

    Update pro_setup_table.php

--- pro_setup_table.php
+++ pro_setup_table.php
@@ -284,9 +284,9 @@ if (isset($_POST['submitBtn'])) {
   <div class="breadCrumb_bar_iner">
     <div class="">
         <div class="btn-group btn-breadcrumb">
-          <a href="#" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
-          <a href="Usermodules.php" class="btn btn-info">Setup</a>
-          <a href="hr_add_emp_info.php" class="btn btn-info active">Commodity Setup</a>
+          <a href="usermodules.php" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
+          <a href="usermodules.php" class="btn btn-info">Setup</a>
+          <a href="pro_setup_table.php" class="btn btn-info active">Commodity Setup</a>
         </div>
     </div>
   </div>
