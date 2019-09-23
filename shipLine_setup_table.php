492c467d41b6b4a201455acd369b4069e262a637
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-23 11:21:11 +0500

    Update shipLine_setup_table.php

--- shipLine_setup_table.php
+++ shipLine_setup_table.php
@@ -190,9 +190,9 @@ if(isset($_POST["btnDelete"]))
   <div class="breadCrumb_bar_iner">
     <div class="">
         <div class="btn-group btn-breadcrumb">
-          <a href="#" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
-          <a href="Usermodules.php" class="btn btn-info">Setups</a>
-          <a href="usertable.php" class="btn btn-info active">Shipping Line Table</a>
+          <a href="usermodules.php" class="btn btn-info "><i class="glyphicon glyphicon-home"></i></a>
+          <a href="usermodules.php" class="btn btn-info">Setups</a>
+          <a href="shipLine_setup_table.php" class="btn btn-info active">Shipping Line Table</a>
         </div>
     </div>
   </div>
