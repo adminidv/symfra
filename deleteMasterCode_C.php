c6904a3f02a9e096631611e97b27a279c9ba754d
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-24 16:11:54 +0500

    Update deleteMasterCode_C.php

--- deleteMasterCode_C.php
+++ deleteMasterCode_C.php
@@ -4,14 +4,31 @@ include('manage/connection.php');
 
 if(isset($_GET["newCode"]))
 {
-  $query = "UPDATE custmaster SET cmpStatus='Inactive' WHERE newCode = '".$_GET["newCode"]."' ";
-  $statement = $con->prepare($query);
-  $statement->execute();
+	$selectStatus = mysqli_query($con, "SELECT * FROM custmaster WHERE newCode='".$_GET["newCode"]."' ");
+    while ($rowStatus = mysqli_fetch_array($selectStatus))
+    {
+      $currentStatus = $rowStatus['cmpStatus'];
+    }
 
-  // echo $_GET["newCode"];
+    if ($currentStatus == "Active")
+    {
+      mysqli_query($con, "UPDATE custmaster SET cmpStatus='Deactive' WHERE newCode = '".$_GET["newCode"]."' ");
+    }
+    else if ($currentStatus == "Deactive")
+    {
+      mysqli_query($con, "UPDATE custmaster SET cmpStatus='Active' WHERE newCode = '".$_GET["newCode"]."' ");
+    }
+
+    header("Location: master_customer_table.php");
+
+  /*$query = "UPDATE custmaster SET cmpStatus='Inactive' WHERE newCode = '".$_GET["newCode"]."' ";
+  $statement = $con->prepare($query);
+  $statement->execute();*/
+
+   // echo $_GET["newCode"];
  
 }
 
-echo "Done";
+// echo "Done";
 
 ?>
