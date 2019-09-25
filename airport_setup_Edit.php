940c6ed62c58a3d22f9bca7c49b95ce5389d93c9
zaid <zaid@bitsnpears.com>
2019-09-25 19:07:25 +0500

    Update airport_setup_Edit.php

--- airport_setup_Edit.php
+++ airport_setup_Edit.php
@@ -93,7 +93,7 @@ while ($rowSrNo = mysqli_fetch_array($selectSrNo))
 
 
 
-	if (isset($_POST['submitBtn'])) {
+  if (isset($_POST['submitBtn'])) {
   $airport_name = $_POST['airport_name'];
   $airport_iata = $_POST['airport_iata'];
   $airport_ICAO = $_POST['airport_ICAO'];
@@ -309,27 +309,27 @@ header("Location: airport_setup_2.php");
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Airport Setup</title>
-	<link rel="shortcut icon" type="image/png" href="./images/favicon.png">
-	<link rel="stylesheet" href="css/bootstrap.css" type="text/css">
-	<link rel="stylesheet" href="css/font-awesome.css" type="text/css">
-	<link rel="stylesheet" type="text/css" href="css/symfra_forms.css">
+  <title>Airport Setup</title>
+  <link rel="shortcut icon" type="image/png" href="./images/favicon.png">
+  <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+  <link rel="stylesheet" href="css/font-awesome.css" type="text/css">
+  <link rel="stylesheet" type="text/css" href="css/symfra_forms.css">
   <link rel="stylesheet" type="text/css" href="css/crm.css">
   <link rel="stylesheet" type="text/css" href="css/main_box_widgets.css">
-	<link rel="stylesheet" type="text/css" href="css/usertable.css">
-	<link rel="stylesheet" type="text/css" href="css/sidebar.css">
-	<link rel="stylesheet" type="text/css" href="css/jquery.dataTables.min.css">
+  <link rel="stylesheet" type="text/css" href="css/usertable.css">
+  <link rel="stylesheet" type="text/css" href="css/sidebar.css">
+  <link rel="stylesheet" type="text/css" href="css/jquery.dataTables.min.css">
 
-	<link rel="stylesheet" href="css/symfra_popups.css" type="text/css">
-	<script src="js/jquery-3.3.1.js"></script>
+  <link rel="stylesheet" href="css/symfra_popups.css" type="text/css">
+  <script src="js/jquery-3.3.1.js"></script>
 
   <!-- Bread crumbs starting here -->
   <link rel="stylesheet" href="css/breadCrumb.css" type="text/css">
   <link rel="shortcut icon" type="image/png" href="images/favicon.png">
   <link rel="stylesheet" href="css/user_ribbon.css" type="text/css">
   <!-- Bread crumbs ending here -->
-	
-  	<script src="js/sidebar.js"></script>
+  
+    <script src="js/sidebar.js"></script>
     <script src="js/jquery.min.js"></script>
     
 
@@ -473,9 +473,9 @@ header("Location: airport_setup_2.php");
 </div>
 
 <div class="  main_widget_box">
-	<div class="">
-									<!-- <hr> -->
-			<form action="" method="POST" enctype="multipart/form-data">
+  <div class="">
+                  <!-- <hr> -->
+      <form action="" method="POST" enctype="multipart/form-data">
 
 
 <!-- Show Log Chain -->
@@ -579,53 +579,53 @@ header("Location: airport_setup_2.php");
               </div>
       </div>
 
-				     	 <!-- Modal_one-->
-			 <div class="modal fade confirmTable-modal" id="saveAirline_Modal" role="dialog">
-			    <div class="modal-dialog">
-			    
-			      <!-- Modal content-->
-			      <div class="modal-content">
-			        <div class="modal-header">
-			          <button type="button" class="close" data-dismiss="modal">&times;</button>
-			          <h4 class="modal-title">Confirmation</h4>
-			        </div>
-			        <div class="modal-body">
-			          <p>Are You Sure You Want to Save?</p>
-			          <button type="submit" name="submitBtn1">Yes</button>
-	                  <button type="button" name="btnDelete_N" data-dismiss="modal" >No</button>
+               <!-- Modal_one-->
+       <div class="modal fade confirmTable-modal" id="saveAirline_Modal" role="dialog">
+          <div class="modal-dialog">
+          
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Confirmation</h4>
+              </div>
+              <div class="modal-body">
+                <p>Are You Sure You Want to Save?</p>
+                <button type="submit" name="submitBtn1">Yes</button>
+                    <button type="button" name="btnDelete_N" data-dismiss="modal" >No</button>
 
-			        </div>
-			        <div class="modal-footer">
-			        	<p>Add Related content if needed</p>
-			          <!-- <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> -->
-			        </div>
-			      </div>
-			      
-			    </div>
-			 </div>
+              </div>
+              <div class="modal-footer">
+                <p>Add Related content if needed</p>
+                <!-- <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> -->
+              </div>
+            </div>
+            
+          </div>
+       </div>
 
-				       <!-- Modal Two-->
-				       <div class="modal fade confirmTable-modal" id="submitAirline_Modal" role="dialog">
-				            <div class="modal-dialog">
-				              <!-- Modal content-->
-				              <div class="modal-content">
-				                <div class="modal-header">
-				                  <button type="button" class="close" data-dismiss="modal">&times;</button>
-				                  <h4 class="modal-title">Confirmation</h4>
-				                </div>
-				                <div class="modal-body">
-				                  <p>Are You Sure You Want to Submit?</p>
-				                  <button type="submit" name="submitBtn">Yes</button>
-				                      <button type="button" name="btnDelete_N" data-dismiss="modal" >No</button>
+               <!-- Modal Two-->
+               <div class="modal fade confirmTable-modal" id="submitAirline_Modal" role="dialog">
+                    <div class="modal-dialog">
+                      <!-- Modal content-->
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <button type="button" class="close" data-dismiss="modal">&times;</button>
+                          <h4 class="modal-title">Confirmation</h4>
+                        </div>
+                        <div class="modal-body">
+                          <p>Are You Sure You Want to Submit?</p>
+                          <button type="submit" name="submitBtn">Yes</button>
+                              <button type="button" name="btnDelete_N" data-dismiss="modal" >No</button>
 
-				                </div>
-				                <div class="modal-footer">
-				                  <p>Add Related content if needed</p>
-				                  <!-- <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> -->
-				                </div>
-				              </div>
-				            </div>
-				       </div>
+                        </div>
+                        <div class="modal-footer">
+                          <p>Add Related content if needed</p>
+                          <!-- <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> -->
+                        </div>
+                      </div>
+                    </div>
+               </div>
 
                <!-- valdition submit popup -->
                <div class="modal fade confirmTable-modal" id="submit_Modal" role="dialog">
@@ -674,7 +674,7 @@ header("Location: airport_setup_2.php");
                </div>
 
 
-				  <div class="modal fade symfra_popup2" id="popupMEdit4" role="dialog">
+          <div class="modal fade symfra_popup2" id="popupMEdit4" role="dialog">
             <div class="modal-dialog">
               <!-- ADD Airport Details-->
               <div class="modal-content">
@@ -781,32 +781,32 @@ header("Location: airport_setup_2.php");
                       </div>
                     </div>
                 </div>   
-          </div>       		      
+          </div>                
 
 
           
 
             
-						<h4><label id="formSummary" style="color: red;"></label></h4>
+            <h4><label id="formSummary" style="color: red;"></label></h4>
              <p id="V_airport_name" style="color: red;"></p>
               <p id="V_airport_ICAO" style="color: red;"></p>
               <p id="V_email" style="color: red;"></p>
               <p id="V_cont_per_off" style="color: red;"></p>
               <p id="V_fax_no" style="color: red;"></p>
-							
+              
 
-											  
-								<div class=" widget_iner_box">
+                        
+                <div class=" widget_iner_box">
 
-											<div class="form_sec_action_btn col-md-12">
-													<div class="form_action_right_btn">
-										                      <!-- Go back button code starting here -->
-										                      <?php include('inc_widgets/backBtn.php'); ?>
-										                      <!-- Go back button code ending here -->
-													</div>
+                      <div class="form_sec_action_btn col-md-12">
+                          <div class="form_action_right_btn">
+                                          <!-- Go back button code starting here -->
+                                          <?php include('inc_widgets/backBtn.php'); ?>
+                                          <!-- Go back button code ending here -->
+                          </div>
                           <!-- log change btn -->
                           <button type="button" name="saveBtn" onclick="logUserFunc();" > <small>Log Chain</small></button>
-													<button type="button" id="btnConfirm_Su" onclick="FormValidation()" > <small>Submit</small></button>
+                          <button type="button" id="btnConfirm_Su" onclick="FormValidation()" > <small>Submit</small></button>
                           <button type="button" name="btnConfirm_S" onclick="saveAirlineFunc();"> <small>Save</small></button>
                           <button type="button" name="cancel"> <small>Cancel</small></button>       
                       </div>
@@ -928,7 +928,7 @@ header("Location: airport_setup_2.php");
                         <div class="cls"></div>
                         <hr>
 
-								
+                
 
 
           <div id="representative" class="tab-pane fade in ">
@@ -1029,12 +1029,12 @@ header("Location: airport_setup_2.php");
                 </div>                    
           </div>
           
-											
-								
-		</form>
-				
+                      
+                
+    </form>
+        
 
-	</div>
+  </div>
 
 </div>
 
@@ -1409,28 +1409,28 @@ header("Location: airport_setup_2.php");
 <script type="text/javascript">
   function saveAirlineFunc()
   {
-  	$("#saveAirline_Modal").modal();
+    $("#saveAirline_Modal").modal();
   }
 </script>
 
 <script type="text/javascript">
-		function readURL(input) 
-		{
-	        if (input.files && input.files[0])
-	        {
-	            var reader = new FileReader();
+    function readURL(input) 
+    {
+          if (input.files && input.files[0])
+          {
+              var reader = new FileReader();
 
-	            reader.onload = function (e)
-	            {
-	                $('#blah')
-	                    .attr('src', e.target.result)
-	                    .width(185)
-	                    .height(185);
-	            };
+              reader.onload = function (e)
+              {
+                  $('#blah')
+                      .attr('src', e.target.result)
+                      .width(185)
+                      .height(185);
+              };
 
-	            reader.readAsDataURL(input.files[0]);
-	        }
-    	}
+              reader.readAsDataURL(input.files[0]);
+          }
+      }
 </script>
 
 <script>
