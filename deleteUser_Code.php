84d6a976899df3d8450ff9f8396506264b9cc686
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-25 14:58:17 +0500

    Update deleteUser_Code.php

--- deleteUser_Code.php
+++ deleteUser_Code.php
@@ -5,7 +5,7 @@ $id = $_GET['id'];
 
 if(isset($_GET['id']))
 {
-	$query = mysqli_query($con, "UPDATE users SET status='Deactive' WHERE user_ID = '".$_GET['id']."' ");
+	$query = mysqli_query($con, "UPDATE users SET user_active='Deactived' WHERE user_ID = '".$_GET['id']."' ");
   	echo $_GET['id'];
  	header("Location: usertable.php");
 }
