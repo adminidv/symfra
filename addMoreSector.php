664bfe03f175dfbfe27e39860633ac05613e5e6c
zaid <zaid@bitsnpears.com>
2019-09-25 15:32:04 +0500

    Update addMoreSector.php

--- addMoreSector.php
+++ addMoreSector.php
@@ -3,6 +3,29 @@
 include('manage/connection.php');
 include("manage/session.php");
 
+// Add Change log
+//login user
+$loginUser1= $_SESSION['user'];
+// Today date func
+$todayDate1 = date("Y-m-d");
+
+// for Change log ID Add
+$selectSrNo = mysqli_query($con, "SELECT * FROM sector_setup ORDER BY SrNo DESC LIMIT 1");
+while ($rowSrNo = mysqli_fetch_array($selectSrNo))
+{
+  $SrNo = $rowSrNo['SrNo'];
+  $newSrNo1 = $SrNo + 1;
+  $SrNo1 = $newSrNo1;
+
+}
+
+// For Change Log Record
+$selectLastID1 = mysqli_query($con, "SELECT * FROM chainlog WHERE record_id = '$SrNo' ORDER BY instance DESC LIMIT 1  ");
+  $rowLastID1 = mysqli_fetch_array($selectLastID1, MYSQLI_ASSOC);
+
+  $lastID1 = $rowLastID1['instance'];
+  $newID1 = $lastID1 + 1;
+  $instance = $newID1;
 // Fetching the variables from coming file
 $sector_name = $_GET['sector_name'];
 
@@ -10,5 +33,8 @@ $sector_name = $_GET['sector_name'];
 // $insertQuery = mysqli_query($con, "INSERT INTO spo_setup (spo_name, spo_description, status) VALUES ('$spo_name', '$spo_description',', 'Active') ") or die(mysqli_error($con));
 
 $insertQuery = mysqli_query($con, "insert into sector_setup (sector_name, status) values ('$sector_name','Active')");
+
+
+ $insertQuery2 = mysqli_query($con, "insert into chainlog (instance, formName, record_id,createBy, createDate) values ('$newID1', 'Sector', '$SrNo1', '$loginUser1', '$todayDate1') ");
 
 ?>
