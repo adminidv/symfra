f13e62ce7ac3fecff3ddffc43b0efb583c0c2dd4
aarijmirza <55489799+aarijmirza@users.noreply.github.com>
2019-09-27 16:56:01 +0500

    Update clearing_agents_VE.php

--- clearing_agents_VE.php
+++ clearing_agents_VE.php
@@ -884,6 +884,10 @@ if ($viewvar==$row['SrNo'])
 
 
               <div class="col-md-12">  
+
+                <div class="user_table-title">
+                <h4>Add Representative</h4>
+              </div>
                 <div class="leave-manage-sec-table widget_iner_box ">
                   <div class="form_sec_action_btn col-md-12">
                     <button type="button" id="myBtn">  <small>Add Representative</small></button>
