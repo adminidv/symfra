ba3b282dc10c2c5db34c2154a6271dc903119a12
Anas <anas@idvtechnologies.com>
2019-10-03 13:06:09 +0500

    Anas ka comment

--- abc.php
+++ abc.php
@@ -1,14 +1,1 @@
-<<<<<<< HEAD
-fsdsgdxvcvccvhvhdgdfsfadac
-
-avbc vcd
-
-
-anas + zaid = Love
-na n bmsnabksdds
-
-
-sdadl\asdl;'asld;'d,',asd'
-=======
-fsdsgdxvcvccv
->>>>>>> 5dad6dd97f82d2b8cca2f6991992c28b5b76f822
+my change
